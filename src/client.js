74723575185574ff95b921a4f55e424a0f114b7b
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-19 16:25:09 -0700

    Insert async function to get firebase object

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import * as sapper from '@sapper/app';
 import firebaseConfig from './firebaseConfig';
 
 const app = firebase.initializeApp(firebaseConfig);
+window.firebase = app;
 window.db = app.firestore();
 sapper.start({
   target: document.querySelector('#sapper'),
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,20 @@
 import firebaseConfig from './firebaseConfig';
 
+export async function firebase() {
+    if (process.browser) {
+        return window.firebase;
+    } else {
+        const firebase = await import('firebase');
+        if (firebase.apps.length == 0) {
+            let app = firebase.initializeApp(firebaseConfig);
+            return app;
+        }
+        else {
+            return firebase.apps[0];
+        }
+    }
+}
+
 export async function firestore() {
     if (process.browser) {
         return window.db;
